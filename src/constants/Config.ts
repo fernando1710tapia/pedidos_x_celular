ac7577082ace1d3a37e23a4acdfe971f6a744a42
ChrisNT23 <105956304+ChrisNT23@users.noreply.github.com>
2026-07-09 23:13:00 -0500

    login generico 3

--- src/constants/Config.ts
+++ src/constants/Config.ts
@@ -1,5 +1,11 @@
 export const API_CONFIG = {
     BASE_URL: 'https://www.supertech.ec:8443/infinityone1/resources',
+    //BASE_URL: 'https://200.93.248.121:8443/infinityone1/resources',
+    //BASE_URL: 'https://infinity.petroleosyservicios.com:8443/infinityone1/resources',
+    //BASE_URL: 'https://infinity.petrolrios.ec:8443/infinityone1/resources',
+    // BASE_URL: 'http://www.supertech.ec:8080/infinityone1/resources',
+    //BASE_URL: 'https://infinity.fenapet.com.ec:8443/infinityone1/resources',
+
     GLOBAL_URLS: [
         'https://infinity.petroleosyservicios.com:8443/infinityone1/resources',
         'http://infinity.petrolrios.ec:8080/infinityone1/resources',
